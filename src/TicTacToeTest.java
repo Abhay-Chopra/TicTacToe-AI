9482789e24fb29a5180adcbd002169df846ae20c
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-01-30 22:12:55 -0700

    Initial TicTacToe.java file that is a tic-tac-toe game.

---
+++ src/TicTacToeTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class for testing the TicTacToe file (JUnit5/Jupiter)
+ * @author Abhay Chopra
+ * @version Testing 1.0
+ */
+class TicTacToeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createBoard() {
+
+    }
+
+    @Test
+    void rowsIn() {
+    }
+
+    @Test
+    void columnsIn() {
+    }
+
+    @Test
+    void canPlay() {
+    }
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void full() {
+    }
+
+    @Test
+    void winInRow() {
+    }
+
+    @Test
+    void winInColumn() {
+    }
+
+    @Test
+    void winInDiagonalBS() {
+    }
+
+    @Test
+    void winInDiagonalFS() {
+    }
+
+    @Test
+    void hint() {
+    }
+
+    @Test
+    void factorial() {
+    }
+}
